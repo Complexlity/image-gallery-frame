12cb284fd2ab7d148d3520bdd8f3207afcbc365b
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-28 16:06:30 +0100

    feat: add fc frames to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,28 +5,28 @@ import "@/globals.css"
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta
           name="description"
           content="A place to discover, create and buy exclusive NFTs."
         />
-        <meta property="og:title" content="CreativeArt Marketplace" />
+        <meta property="og:title" content="Farcaster Gallery" />
         <meta
           property="og:description"
-          content="A place to discover, create and buy exclusive NFTs."
+          content="A place to upload images and gifs to farcaster frames"
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="." />
-        <meta property="og:site_name" content="CreativeArt Marketplace" />
+        <meta property="og:site_name" content="Farcaster Gallery" />
         <meta
           property="og:image"
-          content="https://creative-art-marketplace.vercel.app/og.png"
+          content="https://image-gallery-frame.vercel.app/og.png"
         />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:locale" content="en_US" />
-      </Head>
+      </Head> */}
 
               <Component {...pageProps} />
 
--- pages/gallery/[id].tsx
+++ pages/gallery/[id].tsx
@@ -3,23 +3,29 @@ import {  useRouter } from "next/router"
 
 export default function Page() {
 	const query = useRouter().query
-	const id = query.id
+  const id = query.id
+  const HOST = process.env.NEXT_PUBLIC_HOST
+  console.log({HOST})
 	return (
     <>
       <Head>
         <title>My page title</title>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="http://...image-result.png" />
+        <meta property="fc:frame:image" content={`${HOST}/api/image?id=${id}`} />
+        <meta property="fc:frame:post_url" content={`${HOST}/api/toggle?id=${id}`} />
+        <meta property="fc:frame:button:0" content={`${HOST}/api/toggle?id=${id}`} />
+        <meta property="fc:frame:button:0" content={`Prev`} />
+        <meta property="fc:frame:button:1" content={`Next`} />
         <meta property="og:title" content="My page title" key="title" />
       </Head>
       <Head>
         <meta property="og:title" content="My new title" key="title" />
       </Head>
-      <div>Hello from path id</div>
+      <div>Hello from path {id}</div>
     </>
   );
 }
 
-export function getServerSideProps() {
+// export function getServerSideProps() {
 
-}
+// }
