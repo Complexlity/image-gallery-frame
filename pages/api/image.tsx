1efff89b571b92d364eb7cedb743eec06bdf153e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-28 11:13:51 +0100

    chore: remove unused code

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -1,52 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import sharp from 'sharp';
-import {Poll} from "@/app/types";
-import {kv} from "@vercel/kv";
-import satori from "satori";
-import { join } from 'path';
 import * as fs from "fs";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { join } from 'path';
+import satori from "satori";
+import sharp from 'sharp';
 
 const fontPath = join(process.cwd(), 'Roboto-Regular.ttf')
 let fontData = fs.readFileSync(fontPath)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const pollId = req.query['id']
-        // const fid = parseInt(req.query['fid']?.toString() || '')
-        if (!pollId) {
-            return res.status(400).send('Missing poll ID');
-        }
-
-        let poll: Poll | null = await kv.hgetall(`poll:${pollId}`);
-
-
-        if (!poll) {
-            return res.status(400).send('Missing poll ID');
-        }
-
-        const showResults = req.query['results'] === 'true'
-        // let votedOption: number | null = null
-        // if (showResults && fid > 0) {
-        //     votedOption = await kv.hget(`poll:${pollId}:votes`, `${fid}`) as number
-        // }
-
-        const pollOptions = [poll.option1, poll.option2, poll.option3, poll.option4]
-            .filter((option) => option !== '');
-        const totalVotes = pollOptions
-            // @ts-ignore
-            .map((option, index) => parseInt(poll[`votes${index+1}`]))
-            .reduce((a, b) => a + b, 0);
-        const pollData = {
-            question: showResults ? `Results for ${poll.title}` : poll.title,
-            options: pollOptions
-                .map((option, index) => {
-                    // @ts-ignore
-                    const votes = poll[`votes${index+1}`]
-                    const percentOfTotal = totalVotes ? Math.round(votes / totalVotes * 100) : 0;
-                    let text = showResults ? `${percentOfTotal}%: ${option} (${votes} votes)` : `${index + 1}. ${option}`
-                    return { option, votes, text, percentOfTotal }
-                })
-        };
 
         const svg = await satori(
             <div style={{
@@ -65,24 +27,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     flexDirection: 'column',
                     padding: 20,
                 }}>
-                    <h2 style={{textAlign: 'center', color: 'lightgray'}}>{poll.title}</h2>
-                    {
-                        pollData.options.map((opt, index) => {
-                            return (
-                                <div style={{
-                                    backgroundColor:  showResults ? '#007bff' : '',
-                                    color: '#fff',
-                                    padding: 10,
-                                    marginBottom: 10,
-                                    borderRadius: 4,
-                                    width: `${showResults ? opt.percentOfTotal : 100}%`,
-                                    whiteSpace: 'nowrap',
-                                    overflow: 'visible',
-                                }}>{opt.text}</div>
-                            )
-                        })
-                    }
-                    {/*{showResults ? <h3 style={{color: "darkgray"}}>Total votes: {totalVotes}</h3> : ''}*/}
                 </div>
             </div>
             ,
