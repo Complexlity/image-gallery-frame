77d59ee1a7b0ad813c989f46f969c25a54ad395c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-28 11:05:54 +0100

    chore: clean code

--- pages/api/upload-gallery.ts
+++ pages/api/upload-gallery.ts
@@ -13,12 +13,11 @@ export  default async function handler(req: NextApiRequest, res: NextApiResponse
 	if (req.method === "POST") {
 		//TODO: Verify with zod
 		let values = req.body
-		console.log({ values })
-		console.log(values.galleryId, values.filesToSendToKVStore)
+
 
 		values = JSON.parse(values) as PostBody
 
-		console.log(values.galleryId, values.filesToSendToKVStore)
+
 
 		await kv.hset(values.galleryId, values.filesToSendToKVStore)
 		return res.status(200).json({ result: values })
@@ -31,24 +30,19 @@ export  default async function handler(req: NextApiRequest, res: NextApiResponse
 		const itemNumber = req.query.item  || 0
 		let returnedImage;
 
-		console.log(galleryId)
 		if (!galleryId) {
 			return res.status(422).json({message: "Please Provide a galleryId"})
 		}
-		console.log({sort, itemNumber})
 		const values = await kv.hgetall(galleryId)
 		if (!values) {
 			return res.status(404).json({message: "Items with gallery id not found"})
 		}
 
 
-		console.log(sort)
 		if (sort === "desc") {
-			console.log("Sort is descending")
 			returnedImage = Object.values(values).reverse()[+itemNumber]
 		}
 		else {
-			console.log("Sort is ascending")
 			returnedImage = values[+itemNumber]
 		}
 		return res.status(200).json({ result: returnedImage })
