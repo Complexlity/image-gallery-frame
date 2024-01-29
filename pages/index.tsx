ce530cc2a985f328579c2300a6cee55080493bdb
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-29 16:27:42 +0100

    feat: reduce vercel icon size

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,8 @@ const Home = () => {
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
-          <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-            <VercelLogo className="h-8 sm:h-16 invert p-3 mb-1" />
+          <div className="flex justify-center items-center bg-black rounded-full w-12 h-12  my-8">
+            <VercelLogo className="h-8  invert p-3 mb-1" />
           </div>
           <h1 className="text-lg sm:text-2xl font-bold mb-2">
             Farcaster Gallery
@@ -26,6 +26,7 @@ const Home = () => {
           <h2 className="text-md sm:text-xl mx-4">
             Upload Images To Your Gallery. Max per time (5)
           </h2>
+
           <div className="flex flex-wrap items-center justify-around max-w-4xl my-8 sm:w-full bg-white rounded-md shadow-xl h-full border border-gray-100">
             <GalleryCreateForm />
           </div>
@@ -61,4 +62,7 @@ function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
   );
 }
 
+
 export default Home;
+
+
