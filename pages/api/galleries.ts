1fd125e50d28f356e1aad6e0d59bf077a3120044
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-29 15:52:47 +0100

    feat: update to use ENVI variable

--- pages/api/galleries.ts
+++ pages/api/galleries.ts
@@ -2,10 +2,12 @@ import { kv } from '@vercel/kv'
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const SEVEN_DAYS_IN_MS = 1000 * 60 * 60 * 24 * 7;
+const ENVI = process.env.ENVI ?? "devv"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    let visibility = req.body.visiblity ?? "public"
 	let galleryIds = await kv.zrange(
-    "gallery_by_date",
+    `gallery_by_date:${ENVI}`,
     Date.now(),
     Date.now() - SEVEN_DAYS_IN_MS,
     { byScore: true, rev: true }
