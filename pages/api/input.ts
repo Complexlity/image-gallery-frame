df070eaf252b19ad773ebe5e3709b1c15698876e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-03 13:58:33 +0100

    feat: complete input route

--- pages/api/input.ts
+++ pages/api/input.ts
@@ -3,59 +3,81 @@ import { kv } from "@vercel/kv";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const buttons = ['up', 'down', 'left', 'right', 'square', 'circle', 'triangle', 'x']
+function getUsedButtons(page: number) {
+	let usedButtons;
+				switch (page) {
+					case 1:
+						console.log("I am here")
+						usedButtons = [buttons[0], buttons[1]]
+						break
+					case 2:
+						console.log("I am here")
+						usedButtons = [buttons[2], buttons[3]]
+						break
+					case 3:
+						console.log("I am here")
+						usedButtons = [buttons[4], buttons[5]]
+						break
+					case 4:
+						console.log("I am here")
+						usedButtons = [buttons[6], buttons[7]]
+						break
+	}
+	return usedButtons
+			}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    try {
+		try {
 
 			let buttonId = req.body.untrustedData.buttonIndex || 2;
-			const page = req.query as unknown as number
+			let currentPage = req.query.page as unknown as number
+			let nextPage = +currentPage + 1
 
-			let usedButtons: string[] = []
+			if (currentPage > 5) {
+				currentPage = 1
+				nextPage = 2
+			}
+
 			let buttonsTemplate = ''
 
-			if (page == 1 && buttonId == 2) {
+			if (currentPage > 5 && buttonId == 2) {
 				//End of page. Fetch result
 			}
+			const usedButtons = getUsedButtons(+currentPage) ?? []
 
-			switch (page) {
-				case 1:
-					usedButtons = [buttons[0], buttons[1]]
-				case 2:
-					usedButtons = [buttons[2], buttons[3]]
-				case 3:
-					usedButtons = [buttons[4], buttons[5]]
-				case 4:
-					usedButtons = [buttons[6], buttons[7]]
+			console.log(usedButtons)
 
-			}
 
-			if (page == 5) {
-				buttonsTemplate =
-				`
+
+
+				if (currentPage == 5) {
+					buttonsTemplate =
+						`
 					<meta name="fc:frame:button:1" content="->">
-          <meta name="fc:frame:button:2" content="">
+          <meta name="fc:frame:button:2" content="Fetch">
 				`;
-			}
-			else {
-				buttonsTemplate =
-					`<meta name="fc:frame:button:1" content="->">
+				}
+				else {
+					buttonsTemplate =
+						`<meta name="fc:frame:button:1" content="->">
           ${usedButtons.map((button, index) => {
-						return `
-						<meta name="fc:frame:button:${index + 2}" content="">`
-					})}
+							return `
+						<meta name="fc:frame:button:${index + 2}" content="${button}">`
+						})}
 					<meta name="fc:frame:button:4" content="Clear">
 					`
-			}
+				}
 
 
 
 
-      res.setHeader("Content-Type", "text/html");
-      res.status(200).send(`
+
+				res.setHeader("Content-Type", "text/html");
+				res.status(200).send(`
       <!DOCTYPE html>
       <html>
         <head>
@@ -64,9 +86,8 @@ export default async function handler(
           <meta property="og:image" content="$">
           <meta name="fc:frame" content="vNext">
           <meta name="fc:frame:image" content="">
-          <meta name="fc:frame:post_url" content="${
-            process.env["HOST"]
-          }/api/input?page=${page > 5 ? 1 : page + 1}=">
+          <meta name="fc:frame:post_url" content="${process.env["HOST"]
+					}/api/input?page=${nextPage}">
 					${buttonsTemplate}
         </head>
         <body>
@@ -74,11 +95,12 @@ export default async function handler(
         </body>
       </html>
     `);
-    } catch (error) {
+			}
+		catch (error) {
       console.error(error);
       res.status(500).send("Error generating image");
     }
-  } else {
+} else {
     // Handle any non-POST requests
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
