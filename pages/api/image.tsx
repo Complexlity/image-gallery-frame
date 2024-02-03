94575cb545ddad83629a3806024c0df1fb5dd1b0
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-03 18:25:15 +0100

    debug: log stuff

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -26,6 +26,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     inputAsArray.push(input[i])
   }
 
+  console.log({input})
+  console.log({inputAsArray})
 
 
   try {
