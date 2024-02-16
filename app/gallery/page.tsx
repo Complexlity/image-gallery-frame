0c614955e01cafcdfee894824140fca863069340
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-16 18:04:15 +0100

    chore: change text

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
         <h1 className="text-lg sm:text-2xl font-bold mb-2">
-          Created Galleries
+          Recently Created Galleries
         </h1>
         <div className="flex-1 flex-wrap items-center justify-around max-w-4xl my-8 sm:w-full bg-white rounded-md shadow-xl h-full border border-gray-100">
           {galleryIds.map((id, index) => {
