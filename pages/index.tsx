60f68a770c61c44ce9b732a7f0aa1e232f5e376e
Complexlity <nwalozielijah@gmail.com>
2026-02-21 00:04:27 +0100

    chore: remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,33 +26,12 @@ const Home = ({totalGalleriesCreated}: {totalGalleriesCreated: number}) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <CreateGalleryForm />
       
     </>
   );
 };
-
-function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      aria-label="Vercel Logo"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 19"
-      {...props}
-    >
-      <path
-        clipRule="evenodd"
-        d="M12.04 2L2.082 18H22L12.04 2z"
-        fill="#000"
-        fillRule="evenodd"
-        stroke="#000"
-        strokeWidth="1.5"
-      />
-    </svg>
-  );
-}
 
 
 
